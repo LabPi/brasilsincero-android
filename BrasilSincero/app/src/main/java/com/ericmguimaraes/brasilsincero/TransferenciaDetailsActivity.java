f9a80b1e177e02b9c4d854852cd31cc1a1720c85
Thiago Guimaraes <thiagogsr@gmail.com>
2016-04-14 02:28:12 -0300

    Improve 'transferencias' page

--- BrasilSincero/app/src/main/java/com/ericmguimaraes/brasilsincero/TransferenciaDetailsActivity.java
+++ BrasilSincero/app/src/main/java/com/ericmguimaraes/brasilsincero/TransferenciaDetailsActivity.java
@@ -29,6 +29,15 @@ public class TransferenciaDetailsActivity extends AppCompatActivity implements D
     @Bind(R.id.programaContent)
     public TextView nm_programa;
 
+    @Bind(R.id.situacao)
+    public TextView tx_situacao;
+
+    @Bind(R.id.data)
+    public TextView dt_emissao_dl;
+
+    @Bind(R.id.esfera)
+    public TextView tx_esfera_adm_convenente;
+
     @Bind(R.id.proponenteContent)
     public TextView nm_proponente;
 
@@ -37,6 +46,27 @@ public class TransferenciaDetailsActivity extends AppCompatActivity implements D
 
     @Bind(R.id.valorContent)
     public TextView vl_global;
+
+    @Bind(R.id.banco)
+    public TextView nm_banco_convenio;
+
+    @Bind(R.id.agencia)
+    public TextView cd_agencia_convenio;
+
+    @Bind(R.id.conta_corrente)
+    public TextView nr_conta_corrente_convenio;
+
+    @Bind(R.id.banco_favorecido)
+    public TextView num_banco_favorecido_dl;
+
+    @Bind(R.id.agencia_favorecido)
+    public TextView cd_agencia_favorecido_dl;
+
+    @Bind(R.id.conta_corrente_favorecido)
+    public TextView nr_conta_corrente_fav_dl;
+
+    @Bind(R.id.municipio)
+    public TextView nm_municipio_convenente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,9 +107,19 @@ public class TransferenciaDetailsActivity extends AppCompatActivity implements D
         Gson gson = new Gson();
         Transferencia transferencia = gson.fromJson(json, Transferencia.class);
         nm_programa.setText(transferencia.nm_identif_favorecido_dl);
-        nm_proponente.setText(transferencia.nm_orgao_concedente);
+        tx_situacao.setText(transferencia.tx_situacao);
+        dt_emissao_dl.setText(transferencia.dt_emissao_dl);
+        tx_esfera_adm_convenente.setText("Esfera " + capitalize(transferencia.tx_esfera_adm_convenente.toLowerCase()));
+        nm_proponente.setText(transferencia.nm_ident_convenente);
         nm_respons_proponente.setText(transferencia.nm_orgao_concedente);
-        vl_global.setText(transferencia.vl_bruto_dl);
+        nm_municipio_convenente.setText("Município: " + transferencia.nm_municipio_convenente + "/" + transferencia.uf_convenente);
+        vl_global.setText(transferencia.vl_bruto_dl + " bruto / " + transferencia.vl_liquido_dl + " líquido");
+        nm_banco_convenio.setText("Banco: " + transferencia.nm_banco_convenio);
+        cd_agencia_convenio.setText("Agência: " + transferencia.cd_agencia_convenio);
+        nr_conta_corrente_convenio.setText("Conta Corrente: " + transferencia.nr_conta_corrente_convenio);
+        num_banco_favorecido_dl.setText("Banco: " + transferencia.num_banco_favorecido_dl);
+        cd_agencia_favorecido_dl.setText("Agência: " + transferencia.cd_agencia_favorecido_dl);
+        nr_conta_corrente_fav_dl.setText("Conta Corrente: " + transferencia.nr_conta_corrente_fav_dl);
     }
 
     @Override
@@ -93,5 +133,9 @@ public class TransferenciaDetailsActivity extends AppCompatActivity implements D
     @Override
     public void onListFragmentInteraction(DummyContent.DummyItem item) {
 
+    }
+
+    private String capitalize(final String line) {
+        return Character.toUpperCase(line.charAt(0)) + line.substring(1);
     }
 }
--- BrasilSincero/app/src/main/java/com/ericmguimaraes/brasilsincero/model/Transferencia.java
+++ BrasilSincero/app/src/main/java/com/ericmguimaraes/brasilsincero/model/Transferencia.java
@@ -8,9 +8,19 @@ public class Transferencia {
     public String nm_identif_favorecido_dl;
     public String nm_orgao_concedente;
     public String vl_bruto_dl;
+    public String vl_liquido_dl;
     public String dt_emissao_dl;
     public String nm_municipio_convenente;
     public String uf_convenente;
+    public String tx_situacao;
+    public String tx_esfera_adm_convenente;
+    public String nm_ident_convenente;
+    public String nm_banco_convenio;
+    public String cd_agencia_convenio;
+    public String nr_conta_corrente_convenio;
+    public String num_banco_favorecido_dl;
+    public String cd_agencia_favorecido_dl;
+    public String nr_conta_corrente_fav_dl;
 
     public String header;
 }
